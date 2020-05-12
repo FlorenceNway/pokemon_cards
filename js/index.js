0726b5f30bb7e815fadc95e92e6ae354011eefe1
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-05-12 05:07:31 +0100

    rend all cards

--- js/index.js
+++ js/index.js
@@ -5,22 +5,10 @@ localStorage.setItem("url", "https://api.pokemontcg.io/v1/cards");
 const fetch_url = localStorage.getItem("url")
 
 
-const randomNum = (limit) => {
-	return Math.floor(Math.random() * limit);
-};
-
-
 const getData = async () => {
 	const cards = await fetchData(fetch_url);
 
-	console.log(cards)
-
-	const cardsToRender = [];
-	for (let i = 0; i < 9; i++) {
-		cardsToRender.push(cards[randomNum(cards.length)]);
-	}
-
-	renderCards(cardsToRender);
+	renderCards(cards);
 }
 
 getData()
