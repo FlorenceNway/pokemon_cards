414180c2d5894e4c0bef2c428c1a66e5f0227c86
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-05-12 04:24:29 +0100

    import modules

--- js/index.js
+++ js/index.js
@@ -1,8 +1,5 @@
-import {fetchData} from "./api.js"
-
-const cards_container = document.querySelector(".cards");
-
-// ================================
+import {fetchData} from "./api.js";
+import {renderCards} from "./helpers.js"
 
 localStorage.setItem("url", "https://api.pokemontcg.io/v1/cards");
 const fetch_url = localStorage.getItem("url")
@@ -12,49 +9,18 @@ const randomNum = (limit) => {
 	return Math.floor(Math.random() * limit);
 };
 
-const renderCards = (cards) => {
-	cards_container.innerHTML = "";
-
-	cards.forEach((card) => {
-		const li = document.createElement("li");
-		const image = card.imageUrl
-            
-		li.innerHTML = `
-			<div class='img'>
-				<img src=${image} alt="pokemonCard">
-				<div>
-					<p>${card.name}</p>
-				</div>
-			</div>
-			<div class='overlay'>
-				<div class='text'>
-					<p class="number">Number: ${card.number}</p>
-					<p class="hp">HP: ${card.hp}</p>
-					<p class="types">Types: ${card.types}</p>
-					<p class="weaknesses">Weaknesses: ${card.weaknesses}</p>
-				</div>
-			</div>
-			
-        `;
-		cards_container.appendChild(li);
-	});
-};
 
 const getData = async () => {
 	const cards = await fetchData(fetch_url);
-	
+
 	console.log(cards)
 
 	const cardsToRender = [];
 	for (let i = 0; i < 9; i++) {
 		cardsToRender.push(cards[randomNum(cards.length)]);
-    }
+	}
 
 	renderCards(cardsToRender);
 }
 
 getData()
-
-// button.addEventListener("click", async () => {
-	
-//});
