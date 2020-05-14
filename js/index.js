0a6eed059c8e3d3a795fcb2ff68ebb15a229b5d6
Florence Nway <FlorenceNway@users.noreply.github.com>
2020-05-14 12:54:01 +0100

    filtered pokemon render

--- js/index.js
+++ js/index.js
@@ -1,14 +1,23 @@
 import {fetchData} from "./api.js";
 import {renderCards} from "./render.js"
+import {filteredPokemon} from "./filter.js"
 
 localStorage.setItem("url", "https://api.pokemontcg.io/v1/cards");
 const fetch_url = localStorage.getItem("url")
 
+const input = document.querySelector('input')
 
 const getData = async () => {
 	const cards = await fetchData(fetch_url);
-
 	renderCards(cards);
+
+input.addEventListener("keyup", (event) => {
+	const query = event.target.value.toLowerCase();
+	const filteredCards = filteredPokemon(cards, query);
+	
+	renderCards(filteredCards);
+});
 }
 
 getData()
+
